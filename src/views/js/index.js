205b53cb6f3271dea6c69594bce25a3f3f122c4d
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2024-03-18 15:44:49 -0500

    Logica de envio de emnsajes

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -18,4 +18,22 @@ else {
 
 socketNamespace.on("connect", () =>{
     namespace.textContent = group;
-})
+});
+
+//Programando la lógica de envio de mensajes
+
+const sendMessage = document.querySelector('#sendMessage');
+
+sendMessage.addEventListener("click", ()=>{
+    const message = prompt("Escribe tu mensaje: ");
+    socketNamespace.emit("send message", {
+        message, user
+    });
+});
+
+socketNamespace.on("message", messageData =>{
+    const { user, message } = messageData;
+    const li = document.createElement("li");
+    li.textContent = `${user}: ${message}`;
+    chat.append(li);
+});
